314e1e06e972a9d12a4abf5fc54043370fec6783
parkerpalermo <parker_palermo@mynbps.org>
2020-12-16 17:14:57 +0000

    Fixed category input and started adding ASCII Hangman

--- App.java
+++ App.java
@@ -8,12 +8,38 @@ public class App {
       String wordInput = keyboard.nextLine();
       System.out.println("Would you like to add a category (Answer Y or N)?");
       String addCategory = keyboard.nextLine();
-      String category;
-      if(addCategory == "Y")
+      String category = "";
+      if(addCategory.equals("Y"))
       {
         System.out.println("What would you like to make the category?");
         category = keyboard.nextLine();
       }
+      int wordLength = wordInput.length();
+      int lettersGuessed = 0;
+      while(lettersGuessed != wordLength)
+      {
+        for(int i = 50; i>0; i--)
+        {
+          System.out.println("\n");
+        }
+        System.out.println("Category: " + category);
+
+      }
       
+      
+    }
+
+    public String hangmanASCII(int wrongGuesses)
+    {
+      if(wrongGuesses == 0)
+      {
+        System.out.println("  +---+");
+        System.out.println("  |   |");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("=========");
+      }
     }
 }
