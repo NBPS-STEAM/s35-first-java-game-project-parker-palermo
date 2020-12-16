e076378d43c53dd4580276bd6de1e685bd273906
parkerpalermo <parker_palermo@mynbps.org>
2020-12-16 16:35:37 +0000

    Added word and category inputs

--- App.java
+++ App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+      Scanner keyboard = new Scanner(System.in);
+      System.out.println("Welcome to Hangman!");
+      System.out.println("What word would you like to have the other player(s) guess?");
+      String wordInput = keyboard.nextLine();
+      System.out.println("Would you like to add a category (Answer Y or N)?");
+      String addCategory = keyboard.nextLine();
+      String category;
+      if(addCategory == "Y")
+      {
+        System.out.println("What would you like to make the category?");
+        category = keyboard.nextLine();
+      }
+      
     }
 }
