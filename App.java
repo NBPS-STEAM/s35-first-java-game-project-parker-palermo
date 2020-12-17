9910247cc8f799faea8fd238e37550b25f7f47f1
parkerpalermo <parker_palermo@mynbps.org>
2020-12-17 17:05:35 +0000

    Finished ASCII, started Letters showing up

--- App.java
+++ App.java
@@ -16,6 +16,13 @@ public class App {
       }
       int wordLength = wordInput.length();
       int lettersGuessed = 0;
+      int wrongGuesses = 0;
+      String correctGuesses = "*";
+      String emptyLetter = "*"
+      for(int i = 1; i<wordLength; i++)
+      {
+        emptyLetter + correctGuesses;
+      }
       while(lettersGuessed != wordLength)
       {
         for(int i = 50; i>0; i--)
@@ -23,13 +30,30 @@ public class App {
           System.out.println("\n");
         }
         System.out.println("Category: " + category);
-
+        hangmanASCII(wrongGuesses);
+        System.out.println(correctGuesses);
+        break;
       }
       
       
     }
+    public static <Integer> Indices(String str, char c){
+    List<Integer> list = new ArrayList<>(); // define list
+    
+    String guessString = "******";  
+    for(int i = 0; i < str.length; i++){ // go through each char in str
+        if(str.charAt(i) == c){ // if the chars match
+            String start = guessString.substring(0,i);
+            String end = guessString.substring(i+1);
 
-    public String hangmanASCII(int wrongGuesses)
+            guessString = start + c + end;
+            
+        }
+    }
+    return list; // return list
+}
+
+    public static void hangmanASCII(int wrongGuesses)
     {
       if(wrongGuesses == 0)
       {
@@ -38,6 +62,66 @@ public class App {
         System.out.println("      |");
         System.out.println("      |");
         System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("=========");
+      }
+      else if(wrongGuesses == 1)
+      {
+        System.out.println("  +---+");
+        System.out.println("  |   |");
+        System.out.println("  O   |");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("=========");
+      }
+      else if(wrongGuesses == 2)
+      {
+        System.out.println("  +---+");
+        System.out.println("  |   |");
+        System.out.println("  O   |");
+        System.out.println("  |   |");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("=========");
+      }
+      else if(wrongGuesses == 3)
+      {
+        System.out.println("  +---+");
+        System.out.println("  |   |");
+        System.out.println("  O   |");
+        System.out.println(" /|   |");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("=========");
+      }
+      else if(wrongGuesses == 4)
+      {
+        System.out.println("  +---+");
+        System.out.println("  |   |");
+        System.out.println("  O   |");
+        System.out.println(" /|\\  |");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("=========");
+      }
+      else if(wrongGuesses == 5)
+      {
+        System.out.println("  +---+");
+        System.out.println("  |   |");
+        System.out.println("  O   |");
+        System.out.println(" /|\\  |");
+        System.out.println(" /    |");
+        System.out.println("      |");
+        System.out.println("=========");
+      }
+      else if(wrongGuesses == 6)
+      {
+        System.out.println("  +---+");
+        System.out.println("  |   |");
+        System.out.println("  O   |");
+        System.out.println(" /|\\  |");
+        System.out.println(" / \\  |");
         System.out.println("      |");
         System.out.println("=========");
       }
